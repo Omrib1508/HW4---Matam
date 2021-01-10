f3fce6e38c83a53d3f29d6479e9139378d295371
estellemiara <73658281+estellemiara@users.noreply.github.com>
2021-01-10 17:50:21 +0200

    Update client.c

--- Ex4_205596562_342399128/client/client.c
+++ Ex4_205596562_342399128/client/client.c
@@ -11,3 +11,41 @@
 
 //............................Functions................................//
 //.....................................................................//
+
+int ReadFromArguments(int argc, char*argv[], struct ClientArgs *client_args){
+    if (argc != ARGUMENTS) {
+        printf("Error: invalid number of arguments(%d instead of %d)\n",(argc - 1), ARGUMENTS);
+        exit(EXIT_FAILURE);
+    }
+    
+    else {
+        /* get input file path */
+        client_args->server_ip= *argv[1];
+        client_args->server_port=*argv[2];
+        client_args->username=argv[3];
+        
+        SERVER_ADDRESS_STR=client_args->server_ip;
+        SERVER_PORT=client_args->server_port;
+        USERNAME=*client_args->username;
+        
+        check_error(client_args);
+    }
+    return EXIT_SUCCESS;
+}
+
+int check_error(struct ClientArgs* client_args){
+    if (strlen(client_args->username)>20) {
+        return EXIT_FAILURE;
+    }
+
+    for (int i=0; i<strlen(client_args->username); i++) {
+        if ((client_args->username[i]>='0' && client_args->username[i]<='9')||(client_args->username[i]>='a'&& client_args->username[i]<='z')|| (client_args->username[i]>='A'&& client_args->username[i]<='Z'))  {
+            int t=0;
+        }
+        
+        else{
+            return EXIT_FAILURE;
+        }
+    }
+    return EXIT_SUCCESS;
+}
